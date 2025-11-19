652fe676f77ad13526276288d2a65f78b913de83
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2025-11-19 06:16:41 +0300

    restructure using PDM

---
+++ README.md
@@ -0,0 +1,17 @@
+# nakh-daghestanian-assignment
+
+Parsing Chechen-Russian dictionary data and outputting in the agreed upon format.
+
+## Requirements
+
+* Python
+* PDM (can be installed with `python -m pip install pdm` or through your system's package manager)
+
+## Usage
+
+```bash
+pdm install
+pdm run dev
+```
+
+Outputs `processed.csv`.
--- src/ce_ru_parse/processors/maciev_ce_ru.py
+++ src/ce_ru_parse/processors/maciev_ce_ru.py
@@ -1,29 +1,7 @@
-from typing import Generator
-import requests
 import re
-from pathlib import Path
-import json
-import pandas as pd
+from typing import Generator
 
 columns = ["lemma", "morph1", "morph2", "reference_type", "reference_lemma"]
-
-def get_rows():
-    if Path("wordlist.json").exists():
-        with open("wordlist.json") as f:
-            wordlist = json.load(f)
-    else:
-        URL = "https://raw.githubusercontent.com/laamxo/dosham/refs/heads/main/unified.json"
-        wordlist = requests.get(URL).json()
-        with open("wordlist.json", "w") as f:
-            json.dump(wordlist, f)
-
-    for row in wordlist:
-        yield {
-            "id": row["id"],
-            "word": row["word"],
-            "translate": row["translate"],
-            "source": row["parent"]
-        }
 
 
 def parse_definition(text):
@@ -114,31 +92,3 @@ def process_maciev(row: dict[str, str]) -> Generator[list[str] | None]:
     else:
         # print("failed parse:", string)
         pass
-
-
-if __name__ == "__main__":
-    df: list[list[str]] = []
-    for row in get_rows():
-        match row["source"]:
-            case "maciev_ce_ru":
-                for row_ in process_maciev(row):
-                    if row_:
-                        df.append(row_)
-                continue
-            case _:
-                continue
-    (pd.DataFrame(df, columns=["id_meaning", *columns, "morph_tag", "meaning_ru", "example", "raw"])
-        .sort_values(by=["lemma", "id_meaning"])
-        .drop_duplicates()
-        .to_csv("processed.csv")
-    )
-
-# examples = [
-#     "<i>прил.</i> 1) па́русный; <b>гатанан ке̃ма </b>па́русная ло́дка; 2) полоте́нечный; 3) полотняный; паруси́новый; холщо́вый.",
-#     "<i>прич.</i> ветви́стый.",
-#     "шум (<i>голосов</i>); <b>а̃рахь гӀар</b>-<b>говгӀа яьлла </b>на дворе́ подня́лся шум.",
-#     "1) уси́лие, напо́р; 2) опо́ра, опло́т, <b>Советски Союз </b>– <b>машаран гӀортõ ю </b>Сове́тский Сою́з – опло́т ми́ра.",
-# ]
-
-# for e in examples:
-#     print(parse_definition(e))
